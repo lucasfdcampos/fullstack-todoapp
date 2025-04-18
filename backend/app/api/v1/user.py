237f0109bc81d190e68462372d0172ac0b185b8b
Lucas Fernando de Campos <lucas.campos@freterapido.com>
2025-04-18 08:30:47 -0300

    feat: add logged user route (testing auth)

--- backend/app/api/v1/user.py
+++ backend/app/api/v1/user.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import Session
 from app.schemas.user import UserCreate, UserResponse
 from app.crud.user import create_user, get_user_by_username, get_user_by_email
 from app.db.db import get_db
+from app.security import get_current_user
+from app.models.user import User
 
 router = APIRouter()
 
@@ -26,3 +28,12 @@ def create_user_route(user: UserCreate, db: Session = Depends(get_db)):
         username=new_user.username, 
         email=new_user.email
     )
+
+@router.get("/me", response_model=UserResponse)
+def read_users_me(current_user: User = Depends(get_current_user)):
+    if not current_user:
+        raise HTTPException(
+            status_code=404, 
+            detail="User not found"
+        )
+    return UserResponse.from_orm(current_user)
