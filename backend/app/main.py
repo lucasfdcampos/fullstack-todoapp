824419e4d004db12f9790e007b5253e9dd20707c
Lucas Fernando de Campos <lucas.campos@freterapido.com>
2025-04-18 06:31:38 -0300

    feat: managing routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
+from app.api.v1.routes import router as api_v1_router
 
-app = FastAPI()
+app = FastAPI(title="Todo App", version="0.0.1")
 
-@app.get("/health")
-def healthcheck():
-    return {"status": "ok"}
+app.include_router(api_v1_router, prefix="/api/v1", tags=["v1"])
